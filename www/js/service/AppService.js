d0ac6f9c3734481e29fe3dadc9b533727d12a9ea
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-23 08:36:31 +0700

    update app service

--- www/js/service/AppService.js
+++ www/js/service/AppService.js
@@ -3,7 +3,7 @@
 * @Date:   2016-03-17T21:29:31+07:00
 * @Email:  mufrizalrizki@gmail.com
 * @Last modified by:   rizki
-* @Last modified time: 2016-03-17T21:37:38+07:00
+* @Last modified time: 2016-03-23T08:33:11+07:00
 * @License: apache2
 */
 
