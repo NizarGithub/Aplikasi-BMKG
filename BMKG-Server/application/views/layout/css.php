0072d540c2234debe1a9f6c22426ab3c162fc582
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-26 22:55:23 +0700

    tambah css layout

--- BMKG-Server/application/views/layout/css.php
+++ BMKG-Server/application/views/layout/css.php
@@ -1,3 +1,12 @@
+<!--
+@Author: Rizki Mufrizal <rizki>
+@Date:   2016-03-26T22:49:40+07:00
+@Email:  mufrizalrizki@gmail.com
+@Last modified by:   rizki
+@Last modified time: 2016-03-26T22:55:18+07:00
+@License: apache2
+-->
+
 <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url(); ?>assets/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css" />
 <style type="text/css">
