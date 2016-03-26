cbdd54f29910b8af21e85a14d3ba255123083a67
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-26 23:25:46 +0700

    update service

--- BMKG-Client/www/js/service/AppService.js
+++ BMKG-Client/www/js/service/AppService.js
@@ -3,15 +3,18 @@
 * @Date:   2016-03-17T21:29:31+07:00
 * @Email:  mufrizalrizki@gmail.com
 * @Last modified by:   rizki
-* @Last modified time: 2016-03-23T08:33:11+07:00
+* @Last modified time: 2016-03-26T23:01:45+07:00
 * @License: apache2
 */
 
 angular.module('bmkg')
   .factory('AppService', ['$http', function($http) {
+
+    var baseUrl = 'http://127.0.0.1/';
+
     return {
       getCuaca: function() {
-        return $http.get('data/cuaca.xml');
+        return $http.get(baseUrl + 'BMKG-Server/index.php/api/CuacaRestController/cuaca');
       }
     }
   }]);
