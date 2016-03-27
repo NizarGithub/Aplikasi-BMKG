9fcc45b91171796905cae282a327f07f76538ea0
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-27 23:12:38 +0700

    update cuaca rest controller

--- BMKG-Server/application/controllers/api/CuacaRestController.php
+++ BMKG-Server/application/controllers/api/CuacaRestController.php
@@ -1,41 +1,41 @@
 <?php
 
-/*
-@Author: Rizki Mufrizal <rizki>
-@Date:   2016-03-26T22:26:04+07:00
-@Email:  mufrizalrizki@gmail.com
-@Last modified by:   rizki
-@Last modified time: 2016-03-26T22:48:22+07:00
-@License: apache2
-*/
+/**
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Mar 27, 2016
+ * Time 7:59:53 PM
+ * Encoding UTF-8
+ * Project BMKG-Server
+ * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+require APPPATH . '/libraries/REST_Controller.php';
 
-require APPPATH.'/libraries/REST_Controller.php';
+class CuacaRestController extends REST_Controller {
 
-class CuacaRestController extends REST_Controller
-{
-    public function __construct($config = 'rest')
-    {
-        parent::__construct();
+    public function __construct($config = 'rest') {
         header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding");
+        parent::__construct();
         $this->load->model('Cuaca');
-        $this->load->model('CuacaDetail');
     }
 
-    public function cuaca_get()
-    {
+    public function cuaca_get() {
         $cuaca = $this->Cuaca->ambilDataCuaca()[0];
 
         $tanggal = $cuaca->tanggal;
         $idCuaca = $cuaca->id_cuaca;
 
-        $cuacaDetail = $this->CuacaDetail->ambilDataCuacaDetail($idCuaca);
+        $cuacaDetail = $this->Cuaca->ambilDataCuacaDetail($idCuaca);
 
         $response = array(
-          'tanggal' => $tanggal,
-          'content' => $cuacaDetail
-       );
+            'tanggal' => $tanggal,
+            'content' => $cuacaDetail
+        );
 
         $this->response($response, REST_Controller::HTTP_OK);
     }
+
 }
