59702821a1103413bacbf1420681fcbf66b61be2
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-27 23:13:27 +0700

    pindah cuaca detail ke cuaca

--- BMKG-Server/application/models/Cuaca.php
+++ BMKG-Server/application/models/Cuaca.php
@@ -1,31 +1,38 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
 
-/*
-@Author: Rizki Mufrizal <rizki>
-@Date:   2016-03-26T20:46:31+07:00
-@Email:  mufrizalrizki@gmail.com
-@Last modified by:   rizki
-@Last modified time: 2016-03-26T22:32:43+07:00
-@License: apache2
-*/
+/**
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Mar 27, 2016
+ * Time 7:58:07 PM
+ * Encoding UTF-8
+ * Project BMKG-Server
+ * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+class Cuaca extends CI_Model {
 
-class Cuaca extends CI_Model
-{
-    public function ambilDataCuaca()
-    {
+    public function ambilDataCuaca() {
         $this->db->order_by('tanggal', 'DESC');
         return $this->db->get('tb_cuaca')->result();
     }
 
-    public function ambilDataCuacaBerdasarkanTanggal($tanggal)
-    {
+    public function ambilDataCuacaBerdasarkanTanggal($tanggal) {
         $this->db->where('tanggal', $tanggal);
         return $this->db->get('tb_cuaca')->num_rows();
     }
 
-    public function simpanCuaca($cuaca)
-    {
+    public function simpanCuaca($cuaca) {
         $this->db->insert('tb_cuaca', $cuaca);
     }
+
+    public function ambilDataCuacaDetail($idCuaca) {
+        $this->db->where('id_cuaca', $idCuaca);
+        return $this->db->get('tb_cuaca_detail')->result();
+    }
+
+    public function simpanCuacaDetail($cuacaDetail) {
+        $this->db->insert('tb_cuaca_detail', $cuacaDetail);
+    }
+
 }
