cded183ced2569fef99ea69bc4185dc36343213e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-27 23:14:03 +0700

    update layout js

--- BMKG-Server/application/views/layout/js.php
+++ BMKG-Server/application/views/layout/js.php
@@ -1,10 +1,13 @@
+<!DOCTYPE html>
 <!--
-@Author: Rizki Mufrizal <rizki>
-@Date:   2016-03-26T20:52:22+07:00
-@Email:  mufrizalrizki@gmail.com
-@Last modified by:   rizki
-@Last modified time: 2016-03-26T21:22:46+07:00
-@License: apache2
+
+ Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ Since Mar 27, 2016
+ Time 8:01:46 PM
+ Encoding UTF-8
+ Project BMKG-Server
+ Package Expression package is undefined on line 9, column 12 in Templates/Scripting/EmptyPHPWebPage.php.
+  
 -->
 
 <script src="<?php echo base_url(); ?>assets/js/jquery-1.12.0.min.js" type="text/javascript"></script>
