065033ed67e3d8d57127f8abbc4f9f226d52e473
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-27 14:40:51 +0700

    update cuaca view

--- BMKG-Server/application/views/CuacaView.php
+++ BMKG-Server/application/views/CuacaView.php
@@ -3,7 +3,7 @@
 @Date:   2016-03-26T21:20:50+07:00
 @Email:  mufrizalrizki@gmail.com
 @Last modified by:   rizki
-@Last modified time: 2016-03-26T22:52:21+07:00
+@Last modified time: 2016-03-27T14:38:51+07:00
 @License: apache2
 -->
 
@@ -20,42 +20,38 @@
 
     <div class="container">
 
-      <a href="<?php echo base_url(); ?>index.php/CuacaController/refreshDataCuaca">
-        <button type="button" class="btn btn-default">
-          refresh
-        </button>
-      </a>
+			<a href="<?php echo base_url(); ?>index.php/CuacaController/refreshDataCuaca">
+				<button type="button" class="btn btn-default">
+				  refresh
+				</button>
+			</a>
 
-      <p></p>
+			<p></p>
 
-      <table id="tabel" class="table table-bordered table-hover table-responsive table-striped">
-        <thead>
-          <tr>
-            <th class="text-center">ID Cuaca</th>
-            <th class="text-center">Tanggal</th>
-            <th class="text-center">Aksi</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($cuaca as $c) {?>
-            <tr>
-              <td class="text-center">
-                <?php echo $c->id_cuaca; ?>
-              </td>
-              <td class="text-center">
-                <?php echo $c->tanggal; ?>
-              </td>
-              <td class="text-center">
+			<table id="tabel" class="table table-bordered table-hover table-responsive table-striped">
+	      <thead>
+	        <tr>
+	          <th class="text-center">ID Cuaca</th>
+	          <th class="text-center">Tanggal</th>
+	          <th class="text-center">Aksi</th>
+	        </tr>
+	      </thead>
+	      <tbody>
+					<?php foreach ($cuaca as $c) { ?>
+						<tr>
+							<td class="text-center"><?php echo $c->id_cuaca; ?></td>
+							<td class="text-center"><?php echo $c->tanggal; ?></td>
+							<td class="text-center">
                 <a href="<?php echo base_url(); ?>index.php/CuacaController/lihatCuacaDetail/<?php echo $c->id_cuaca ?>">
                   <button type="button" class="btn btn-default">
                     Detail
                   </button>
                 </a>
-              </td>
-            </tr>
-            <?php } ?>
-        </tbody>
-      </table>
+							</td>
+						</tr>
+					<?php } ?>
+	      </tbody>
+	    </table>
     </div>
 
     <?php $this->load->view('layout/js')?>
