7276b01a108a468f27b4e7fe3e4db80889f5a2c4
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-27 23:13:12 +0700

    update gempa model

--- BMKG-Server/application/models/Gempa.php
+++ BMKG-Server/application/models/Gempa.php
@@ -1,36 +1,32 @@
 <?php
 
-defined('BASEPATH') or exit('No direct script access allowed');
-/*
-@Author: Rizki Mufrizal <rizki>
-@Date:   2016-03-27T12:27:14+07:00
-@Email:  mufrizalrizki@gmail.com
-@Last modified by:   rizki
-@Last modified time: 2016-03-27T14:28:17+07:00
-@License: apache2
-*/
+/**
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * Since Mar 27, 2016
+ * Time 7:58:24 PM
+ * Encoding UTF-8
+ * Project BMKG-Server
+ * Package Expression package is undefined on line 12, column 14 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+class Gempa extends CI_Model {
 
-class Gempa extends CI_Model
-{
-    public function ambilDataGempa()
-    {
+    public function ambilDataGempa() {
         $this->db->order_by('tanggal', 'DESC');
-
         return $this->db->get('tb_gempa')->result();
     }
 
-    public function ambilDataGempaBerdasarkanTanggalDanJam($tanggal, $jam, $latitude, $longitude)
-    {
+    public function ambilDataGempaBerdasarkanTanggalDanJam($tanggal, $jam, $latitude, $longitude) {
         $this->db->where('tanggal', $tanggal);
         $this->db->where('jam', $jam);
         $this->db->where('latitude', $latitude);
         $this->db->where('longitude', $longitude);
-
         return $this->db->get('tb_gempa')->num_rows();
     }
 
-    public function simpanGempa($gempa)
-    {
+    public function simpanGempa($gempa) {
         $this->db->insert('tb_gempa', $gempa);
     }
+
 }
