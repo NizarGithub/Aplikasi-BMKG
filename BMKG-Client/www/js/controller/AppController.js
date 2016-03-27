e4dded522e5a78274ec953dbed922339582147aa
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-27 23:14:49 +0700

    update url

--- BMKG-Client/www/js/controller/AppController.js
+++ BMKG-Client/www/js/controller/AppController.js
@@ -3,7 +3,7 @@
 * @Date:   2016-03-17T21:29:48+07:00
 * @Email:  mufrizalrizki@gmail.com
 * @Last modified by:   rizki
-* @Last modified time: 2016-03-27T11:30:00+07:00
+* @Last modified time: 2016-03-27T22:25:23+07:00
 * @License: apache2
 */
 
@@ -12,7 +12,8 @@ angular.module('bmkg')
 
     $scope.dataCuaca = {};
     $scope.tanggal = {};
-    $scope.url = 'http://192.168.2.116/BMKG-Server/assets/img/';
+    //$scope.url = 'http://192.168.2.116/BMKG-Server/assets/img/';
+    $scope.url = 'https://bmkg-mufrizal.rhcloud.com/assets/img/';
 
     function getCuaca() {
       AppService.getCuaca().success(function(data) {
