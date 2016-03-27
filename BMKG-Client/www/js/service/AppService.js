7634be672f3d7697d517d3128a87aef0d603ecbd
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-03-27 23:14:58 +0700

    update url

--- BMKG-Client/www/js/service/AppService.js
+++ BMKG-Client/www/js/service/AppService.js
@@ -3,20 +3,24 @@
 * @Date:   2016-03-17T21:29:31+07:00
 * @Email:  mufrizalrizki@gmail.com
 * @Last modified by:   rizki
-* @Last modified time: 2016-03-27T16:17:39+07:00
+* @Last modified time: 2016-03-27T22:35:27+07:00
 * @License: apache2
 */
 
 angular.module('bmkg')
   .factory('AppService', ['$http', function($http) {
-    var baseUrl = 'http://192.168.2.116';
+    //var baseUrl = 'http://192.168.2.116';
+    var baseUrl = 'https://bmkg-mufrizal.rhcloud.com';
 
     return {
       getCuaca: function() {
-        return $http.get(baseUrl + '/BMKG-Server/index.php/api/CuacaRestController/cuaca/');
+        //return $http.get(baseUrl + '/BMKG-Server/index.php/CuacaRestController/cuaca/');
+        //return $http.get(baseUrl + '/index.php/CuacaRestController/cuaca/');
+        return $http.get(baseUrl + '/api/cuaca');
       },
       getGempa: function() {
-        return $http.get(baseUrl + '/BMKG-Server/index.php/api/GempaRestController/gempa/');
+        //return $http.get(baseUrl + '/BMKG-Server/index.php/GempaRestController/gempa/');
+        return $http.get(baseUrl + '/api/gempa');
       }
     }
   }]);
