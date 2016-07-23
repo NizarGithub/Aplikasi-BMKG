857190b99a77e9e024cc80994ddd422d8a188eab
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-23 15:41:54 +0700

    tambah google map dengan API KEY

--- BMKG-Client/www/js/controller/GempaController.js
+++ BMKG-Client/www/js/controller/GempaController.js
@@ -12,12 +12,13 @@ angular.module('bmkg')
 
     $scope.dataGempa = {};
 
+    $scope.googleMapsUrl = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCaNal1EYdElqEZdB-eGlwlJ_7WEZM4atA';
+
     function getGempa() {
       AppService.getGempa().success(function(data) {
         $scope.dataGempa = data.gempa[0];
         $scope.longitude = data.gempa[0].point.coordinates.split(",")[0];
         $scope.latitude = data.gempa[0].point.coordinates.split(",")[1];
-        console.log($scope.longitude);
       });
     }
 
